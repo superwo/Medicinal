3e2bbaac238634e3574c86bbf53da55fbb87b348
avadarad <avadanii@tiscali.it>
2017-08-03 06:08:24 +0300

    added about section

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,11 @@
 $(function() {
 
-	// Custom JS
-	
+	// Services Items
+	var servicesItems = $('.services__item');
+
+	servicesItems.on('click',  function(event) {
+		servicesItems.removeClass('services__item--active');
+		$(this).addClass('services__item--active');
+	});
+
 });
