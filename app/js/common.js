e583f41c60458d606d9f0998633e90e0c6769229
avadarad <avadanii@tiscali.it>
2017-08-09 07:44:04 +0300

    made banner carousel

--- app/js/common.js
+++ app/js/common.js
@@ -8,11 +8,26 @@ $(function() {
 		$(this).addClass('services__item--active');
 	});
 
+	//carousel section
 	var brands = $('.brands__carousel');
+	var banner = $('.main-banner__carousel');
+
 	brands.owlCarousel({
 		items: 5,
 		autoplay: true
 	});
+	banner.owlCarousel({
+		items: 1,
+		autoplay: true,
+		loop: true,
+		smartSpeed: 4000,
+		autoplayTimeout: 10000,
+		nav: true,
+		autoplayHoverPause: true,
+		navText: ['&#xf062;', '&#xf063;']
+		// autoplaySpeed: 5000
+	});
+
 
 	var upButton = $('.up-button');
 	upButton.on('click', function(event) {
